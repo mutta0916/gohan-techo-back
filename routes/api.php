3bec6024c7ff4d6bf32c1e5488ca019b6776a312
mutta0916 <mutta0916@gmail.com>
2022-01-15 13:58:02 +0900

    CORSエラー対応

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function () {
+  $message = 'hello!api!';
+
+  return response()->json([
+      'message' => $message
+  ]);
+});
