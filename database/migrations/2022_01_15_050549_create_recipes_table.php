4a1e49b77216ebd2ca30c85ae3a21126f55bf7dc
mutta0916 <mutta0916@gmail.com>
2022-01-18 20:28:38 +0900

    マイグレーションファイル作成

---
+++ database/migrations/2022_01_15_050549_create_recipes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('user_id');
+            $table->string('name');
+            $table->tinyInteger('genre');
+            $table->tinyInteger('type');
+            $table->bigIncrements('servings');
+            $table->bigIncrements('photo');
+            $table->bigIncrements('memo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
