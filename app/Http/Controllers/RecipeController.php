b6d05536f3a3d8ce0793c136f86ed4779db81890
mutta0916 <mutta0916@gmail.com>
2022-01-27 00:34:05 +0900

    料理手順追加処理実装

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Recipe;
 use App\RecipeGenre;
+use App\RecipeHowto;
 use App\RecipeType;
 use SebastianBergmann\Environment\Console;
 
@@ -25,18 +26,37 @@ class RecipeController extends Controller
 
     public function store(Request $request)
     {
+        Log::info('★★★登録処理です★★★');
         $recipe = new Recipe;
-        // $form = $request->all();
-        //$form = $request->input('name');
         $recipe->user_id = 1;
         $recipe->name = $request->input('name');
         $recipe->genre_id = $request->input('genre');
         $recipe->type_id = $request->input('type');
-        // $recipe->servings = $request->input('name');
-        // $recipe->memo = $request->input('name');
-        Log::info('★★★登録処理です★★★');
-        // $recipe->fill($form)->save();
+        $recipe->servings = $request->input('servings');
+        $recipe->memo = $request->input('memo');
         $recipe->save();
+
+        // 料理手順テーブルデータ追加
+        $id = $recipe->id;
+        $recipeHowto = new RecipeHowto;
+        $arrHowto = $request->input('howto');
+        Log::info($arrHowto);
+        foreach($arrHowto as $value){
+          Log::info('配列取得');
+          Log::info($value);
+          Log::info('ID取得');
+          Log::info($value->id);
+          Log::info('作り方取得');
+          Log::info($value->howto);
+          $recipeHowto->user_id = 1;
+          $recipeHowto->recipe_id = $id;
+          $recipeHowto->howto_id = $value->id;
+          $recipeHowto->howto = $value;
+          // $recipeHowto->save();
+        }
+
+        // $recipe->fill($form)->save();
+
         $recipes = Recipe::all();
         return response()->json([
             'message' => 'Recipe created successfully',
