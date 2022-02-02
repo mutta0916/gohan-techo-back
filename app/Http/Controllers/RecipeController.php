fbea3cd8073b1eee734ebddce2d0e729e9de5548
mutta0916 <mutta0916@gmail.com>
2022-02-02 19:44:48 +0900

    料理登録処理実装

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -11,6 +11,7 @@ use App\RecipeHowto;
 use App\RecipeIngredient;
 use App\RecipeType;
 use SebastianBergmann\Environment\Console;
+use Illuminate\Support\Facades\Storage;
 
 class RecipeController extends Controller
 {
@@ -28,32 +29,34 @@ class RecipeController extends Controller
     public function store(Request $request)
     {
         // 料理テーブル更新
-        Log::info('登録処理');
         $recipe = new Recipe;
+        $userId = $request->input('user_id');
         $recipeParams = $request->only(['name', 'genre_id', 'type_id', 'servings', 'memo']);
         $recipeParams = array_merge($recipeParams,array('user_id' => 1));
         $recipe->fill($recipeParams)->save();
 
         // 料理手順テーブル更新
         $recipeId = $recipe->id;
-        $arrHowto = $request->input('howto');
+        $arrHowto = json_decode($request->input('howto'));
         foreach($arrHowto as $value){
           $recipeHowto = new RecipeHowto;
-          $howtoParams = array('user_id' => 1, 'recipe_id' => $recipeId, 'howto_id' => $value['id'], 'howto' => $value['howto']);
+          $howtoParams = array('user_id' => $userId, 'recipe_id' => $recipeId, 'howto_id' => $value->id, 'howto' => $value->howto);
           $recipeHowto->fill($howtoParams)->save();
         }
 
         // 料理材料テーブル更新
-        $arrIngredient = $request->input('ingredient');
+        $arrIngredient = json_decode($request->input('ingredient'));
         foreach($arrIngredient as $value){
           $recipeIngredient = new RecipeIngredient;
-          $ingredientParams = array('user_id' => 1, 'recipe_id' => $recipeId, 'ingredient_id' => $value['id'], 'name' => $value['name'], 'amount' => $value['amount']);
+          $ingredientParams = array('user_id' => $userId, 'recipe_id' => $recipeId, 'ingredient_id' => $value['id'], 'name' => $value['name'], 'amount' => $value['amount']);
           $recipeIngredient->fill($ingredientParams)->save();
         }
 
         // 料理写真取得
-        $photo = $request->input('photo');
-        Log::info($photo);
+        $photo = $request->file('photo');
+        $file_name = sprintf('%s%s%s', $userId, '/', $recipeId);
+        $disk = Storage::disk('s3');
+        $disk->put($file_name, $photo, 'public');
 
         return response()->json([
             'message' => 'Recipe created successfully'
